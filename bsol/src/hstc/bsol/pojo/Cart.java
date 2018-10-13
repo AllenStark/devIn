0168d14a511629000face9e9a19cddcfbb7c7cdd
Lynn-yi <43873602+Lynn-yi@users.noreply.github.com>
2018-10-13 21:38:10 +0800

    Add files via upload

--- bsol/src/hstc/bsol/pojo/Cart.java
+++ bsol/src/hstc/bsol/pojo/Cart.java
@@ -1,47 +1,36 @@
 package hstc.bsol.pojo;
 
+import java.text.DecimalFormat;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 public class Cart {
-	private int	userid;
-	private long isbn;
-	private int cartitemcount;
-
-	public Cart() {}
-
-	public Cart(int userid, long isbn, int cartitemcount) {
-		super();
-		this.userid = userid;
-		this.isbn = isbn;
-		this.cartitemcount = cartitemcount;
-	}
-
-	public int getUserid() {
-		return userid;
-	}
-
-	public void setUserid(int userid) {
-		this.userid = userid;
-	}
-
-	public long getIsbn() {
-		return isbn;
-	}
-
-	public void setIsbn(long isbn) {
-		this.isbn = isbn;
-	}
-
-	public int getCartitemcount() {
-		return cartitemcount;
-	}
-
-	public void setCartitemcount(int cartitemcount) {
-		this.cartitemcount = cartitemcount;
-	}
-
-	@Override
-	public String toString() {
-		return "Cart [userid=" + userid + ", isbn=" + isbn + ", cartitemcount=" + cartitemcount + "]";
-	}
-	
-	
+	//使用map集合的目的就是为了避免在购物车中出现重复的商品
+		private Map<Long,CartItem> map = new LinkedHashMap<>();
+		//记录购物车中所有商品的总价格
+		private double countPrice;
+		public Map<Long, CartItem> getMap() {
+			return map;
+		}
+		public void setMap(Map<Long, CartItem> map) {
+			this.map = map;
+		}
+		//得到购物车中商品的总价格
+		public double getCountPrice() {
+			double totalPrice = 0;
+			for(Entry<Long,CartItem> entry : map.entrySet()){
+				CartItem item = entry.getValue();
+				totalPrice += item.getSubtotal();
+				//数字格式化操作
+				DecimalFormat df = new DecimalFormat("0.00");
+				String strTotalPrice = df.format(totalPrice);
+				totalPrice = Double.parseDouble(strTotalPrice);
+			}
+			this.countPrice = totalPrice;
+			return countPrice;
+		}
+		public void setCountPrice(double countPrice) {
+			this.countPrice = countPrice;
+		}
 }
---
+++ bsol/src/hstc/bsol/pojo/CartItem.java
@@ -0,0 +1,87 @@
+package hstc.bsol.pojo;
+
+import java.text.DecimalFormat;
+
+import hstc.bsol.service.BookService;
+
+public class CartItem {
+	private int	userid;
+	private long isbn;
+	private int cartitemcount;
+	private double subtotal;
+	private Book book;
+	public Book getBook() {
+		BookService bookService = new BookService();
+		book = bookService.queryOneBookByIsbn(isbn);
+		return book;
+	}
+	
+	public double getSubtotal() {
+		DecimalFormat df = new DecimalFormat("0.00");
+		double subtotal = this.book.getPrice()*this.cartitemcount;
+		String strSubtotal = df.format(subtotal);
+		subtotal = Double.parseDouble(strSubtotal);
+		return subtotal;
+	}
+
+	public void setSubtotal(double subtotal) {
+		this.subtotal = subtotal;
+	}
+
+	public void setBook(Book book) {
+		this.book = book;
+	}
+
+	public int getUserid() {
+		return userid;
+	}
+
+	public void setUserid(int userid) {
+		this.userid = userid;
+	}
+
+	public long getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(long isbn) {
+		this.isbn = isbn;
+	}
+
+	public int getCartitemcount() {
+		return cartitemcount;
+	}
+
+	public void setCartitemcount(int cartitemcount) {
+		this.cartitemcount = cartitemcount;
+	}
+
+	public CartItem() {
+		super();
+	}
+
+	public CartItem(int userid, long isbn, int cartitemcount) {
+		this.userid = userid;
+		this.isbn = isbn;
+		this.cartitemcount = cartitemcount;
+	}
+	
+	public CartItem(int cartitemcount, int userid, long isbn) {
+		super();
+		this.userid = userid;
+		this.isbn = isbn;
+		this.cartitemcount = cartitemcount;
+	}
+
+	public CartItem(int userid, long isbn) {
+		this.userid = userid;
+		this.isbn = isbn;
+	}
+
+	@Override
+	public String toString() {
+		return "CartItem [userid=" + userid + ", isbn=" + isbn + ", cartitemcount=" + cartitemcount + ", subtotal="
+				+ subtotal + ", book=" + book + "]";
+	}
+	
+}
