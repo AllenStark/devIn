487cad0d0e739d37171b1e4ce1dc87e95cf6dcba
Lynn-yi <43873602+Lynn-yi@users.noreply.github.com>
2018-10-17 01:08:32 +0800

    订单操作的所有业务逻辑层

--- bsol/src/hstc/bsol/service/OrdersService.java
+++ bsol/src/hstc/bsol/service/OrdersService.java
@@ -1,10 +1,43 @@
 package hstc.bsol.service;
 
+import java.util.List;
+
 import hstc.bsol.dao.OrdersDao;
 import hstc.bsol.dao.impl.OrdersDaoImpl;
 import hstc.bsol.pojo.Orders;
 
 public class OrdersService {
+	OrdersDaoImpl ordersDaoImpl = null;
+
+	public OrdersService() {
+		ordersDaoImpl = new OrdersDaoImpl();
+	}
+
+	// 根据用户id查询所有订单的记录数
+	public int queryOrderCountsByUserid(int userid) {
+		return ordersDaoImpl.queryOrderCountsByUserid(userid);
+	}
+
+	// 根据用户id分页查询所有订单信息
+	public List<Orders> queryAllOrdersByUserid(int userid, int pageNo, int pageSize) {
+		return ordersDaoImpl.queryAllOrdersByUserid(userid, pageNo, pageSize);
+	}
+
+	// 根据用户id和订单状态获取该状态下的订单数量
+	public int queryOrdersCountByUseridAndOrderstatus(int userid, int orderstatus) {
+		return ordersDaoImpl.queryOrdersCountByUseridAndOrderstatus(userid, orderstatus);
+	}
+
+	// 根据订单号取消订单
+	public boolean cancelOrderByOrderid(int orderid) {
+		return ordersDaoImpl.cancelOrderByOrderid(orderid);
+	}
+
+	// 根据订单号删除订单
+	public boolean deleteOrderByOrderid(int orderid) {
+		return ordersDaoImpl.deleteOrderByOrderid(orderid);
+	}
+	
 	/**
 	 * 插入一条订单记录
 	 * @param orders
