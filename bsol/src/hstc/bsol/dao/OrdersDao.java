674ac282e2b0b374527a3a2240bbd3982ee90572
Lynn-yi <43873602+Lynn-yi@users.noreply.github.com>
2018-10-17 01:05:19 +0800

    订单的所有dao层

--- bsol/src/hstc/bsol/dao/OrdersDao.java
+++ bsol/src/hstc/bsol/dao/OrdersDao.java
@@ -1,8 +1,32 @@
 package hstc.bsol.dao;
 
+import java.util.List;
+
 import hstc.bsol.pojo.Orders;
 
 public interface OrdersDao {
+	// 根据用户id查询所有订单的记录数
+	public int queryOrderCountsByUserid(int userid);
+	
+	// 根据用户id分页查询所有订单信息
+	/**
+	 * 
+	 * @param userid :用户id
+	 * @param pageNo: 页码数
+	 * @param pageSize: 每页显示的条目数
+	 * @return
+	 */
+	public List<Orders> queryAllOrdersByUserid(int userid,int pageNo,int pageSize);
+
+	// 根据用户id和订单状态获取该状态下的订单数量
+	public int queryOrdersCountByUseridAndOrderstatus(int userid,int orderstatus);
+	
+	//根据订单号取消订单
+	public boolean cancelOrderByOrderid(int orderid);
+	
+	//根据订单号删除订单
+	public boolean deleteOrderByOrderid(int orderid);
+	
 	/**
 	 * 插入一条订单记录
 	 * @param orders
